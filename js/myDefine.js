e6c428a134dcdb347ba8c8045c8008b97a17c08e
NeserCode <73767966+NeserCode@users.noreply.github.com>
2021-12-02 20:24:14 +0800

    first upload
    上传所有内容。
    upload all contents.

---
+++ js/myDefine.js
@@ -0,0 +1,162 @@
+class Fcfs {
+	name = new Array()
+	pid = new Array()
+	t_arrive = new Array()
+	t_service = new Array()
+	t_start = new Array()
+	t_finish = new Array()
+	t_round = new Array()
+	t_round_vex = new Array()
+	finishArray = new Array()
+
+	constructor(arg) {
+		console.log('正在创建的是： 先来先服务 （FCFS）')
+	}
+
+	init(name, t_arr, t_ser, pid) {
+
+		this.name = name
+		this.t_arrive = t_arr
+		this.t_service = t_ser
+		this.pid = pid
+
+		let arr = new Array(),
+			result = new Array()
+
+
+		console.time("fcfs")
+
+		for (let i = 0; i < this.name.length; i++) {
+			let b = new Array();
+			b[0] = this.name[i]
+			b[1] = this.t_arrive[i]
+			b[2] = this.t_service[i]
+			arr[i] = b
+		}
+		arr.sort((a, b) => a[1] - b[1])
+
+		for (let i = 0; i < this.name.length; i++) {
+			let b = new Array()
+			b[0] = parseFloat(Number(i == 0 ? arr[i][1] : arr[i - 1][4]).toFixed(2))
+			b[1] = parseFloat(Number(arr[i][2] + b[0]).toFixed(2))
+			b[2] = parseFloat(Number(b[1] - arr[i][1]).toFixed(2))
+			b[3] = parseFloat(Number(b[2] / arr[i][2]).toFixed(2))
+
+			arr[i] = arr[i].concat(b)
+			this.finishArray.push(arr[i][0])
+			this.t_start.push(b[0])
+			this.t_finish.push(b[1])
+			this.t_round.push(b[2])
+			this.t_round_vex.push(b[3])
+		}
+
+		result = arr
+
+		console.log('↓ 本算法程序用时 ↓');
+		console.timeEnd("fcfs")
+		return {
+			resultArray: result,
+			finishArray: this.finishArray
+		}
+	}
+
+	showMessage() {
+		console.log(`您创建了一个实例，此实例将用作演示 先来先服务算法(FCFS)`)
+		console.log(`调用该实例的 INIT（）函数进行一次调度算法模拟,也可以在控制台中输入 Fcfs 查看全局变量。`)
+		console.log(`例如 \nlet a = new Fcfs()\n之后在控制台键入 a 即可看到返回值`)
+	}
+
+}
+
+class TimePiece {
+
+	time_peice = new Number()
+
+	name = new Array()
+	pid = new Array()
+	t_arrive = new Array()
+	t_service = new Array()
+	t_start = new Array()
+	t_runin = new Array()
+	t_subService = new Array()
+	t_finish = new Array()
+	finishArray = new Array()
+
+	constructor(arg) {
+		console.log('正在创建的是： 时间片轮转调度 （TimePiece）');
+	}
+
+	init(name, t_arr, t_ser, pid, tp) {
+
+		this.name = name
+		this.t_arrive = t_arr
+		this.t_service = t_ser
+		this.pid = pid
+		this.time_peice = tp
+
+		let arr = new Array(),
+			result = new Array(),
+			finish_num = 0,
+			k = 0,
+			time_counter = 0
+
+		console.time("timepiece")
+
+		for (let i = 0; i < this.name.length; i++) {
+			let b = new Array()
+
+			// 进程名|到达时间|服务时间
+
+			b[0] = this.name[i]
+			b[1] = this.t_arrive[i]
+			b[2] = this.t_service[i]
+			b[3] = false
+			arr[i] = b
+		}
+		//将数据由到达时间排序
+		arr.sort((a, b) => a[1] - b[1])
+
+		while (finish_num < this.name.length) {
+			for (let i = 0; i < this.name.length; i++) {
+				let b = new Array()
+				if (!arr[i][3]) {
+
+					// Array b[] 进程名|开始时间|运行时间|剩余服务时间|结束时间
+					// Array arr[][] 进程名|到达时间|服务时间|FinishStatus(true/false)
+
+					b[0] = arr[i][0]
+					b[1] = k == 0 ? arr[0][1] : result[k - 1][4]
+					b[2] = arr[i][2] >= tp ? tp : arr[i][2]
+					b[3] = arr[i][2] - b[2]
+					b[4] = b[1] + b[2]
+					if (b[3] <= 0) {
+						arr[i][3] = true
+						this.finishArray.push(arr[i][0])
+						finish_num++
+					}
+					arr[i][2] -= b[2]
+					result.push(b)
+					this.t_start.push(b[1])
+					this.t_runin.push(b[2])
+					this.t_subService.push(b[3])
+					this.t_finish.push(b[4])
+					k++
+				}
+
+			}
+		}
+		console.log('↓ 本算法程序用时 ↓');
+		console.timeEnd("timepiece")
+		return {
+			resultArray: result,
+			finishArray: this.finishArray
+		}
+	}
+
+	showMessage() {
+		console.log(`您创建了一个实例，此实例将用作演示 时间片轮转算法(TimePiece)`);
+		console.log(`调用该实例的 INIT（）函数进行一次调度算法模拟,也可以在控制台中输入 TimePiece 查看全局变量。`);
+		console.log(`例如 \nlet a = new TimePiece()\n之后在控制台键入 a 即可看到返回值`);
+	}
+
+}
---
+++ js/myVue.js
@@ -0,0 +1,145 @@
+const App = {
+	data() {
+		return {
+			counter: 1,
+			testCase_pid: [3112, 4315, 5241, 5661, 5683],
+			testCase_name: ['进程1', '进程2', '进程3', '进程4', '进程5'],
+			testCase_arriveTime: [3, 2, 4, 6, 5],
+			testCase_serviceTime: [4, 5, 6, 4, 5],
+			time_piece: 2,
+			mainObj: {
+				type: null,
+				res: null,
+				tips: [],
+			},
+			isShowTestCase: true,
+		}
+	},
+	mounted() {
+		this.InitApp()
+		this.InitTestCase()
+	},
+	computed: {
+		type_display: function() {
+			return this.mainObj.type ?? "↓ 您还没有选择一种算法 ↓"
+		}
+	},
+	methods: {
+		InitApp() {
+			console.log(`Vue init ${this.counter?'√':'×'}`)
+		},
+		InitTestCase() {
+			console.log("本实验测试用例如下");
+			console.log('进程名称', this.testCase_name);
+			console.log('到达时间', this.testCase_arriveTime);
+			console.log('服务时间', this.testCase_serviceTime);
+		},
+		resetTestCase() {
+			let testCase_pid = [3112, 4315, 5241, 5661, 5683],
+				testCase_name = ['进程1', '进程2', '进程3', '进程4', '进程5'],
+				testCase_arriveTime = [3, 2, 4, 6, 5],
+				testCase_serviceTime = [4, 5, 6, 4, 5],
+				time_piece = 2
+
+			this.testCase_pid = testCase_pid
+			this.testCase_name = testCase_name
+			this.testCase_arriveTime = testCase_arriveTime
+			this.testCase_serviceTime = testCase_serviceTime
+			this.time_piece = time_piece
+
+			console.log("实验用例已重置");
+
+		},
+		insertTestCase() {
+			this.testCase_pid.push(null)
+			this.testCase_name.push(null)
+			this.testCase_arriveTime.push(null)
+			this.testCase_serviceTime.push(null)
+		},
+		InitFcfs(arr_name, arr_t_arrive, arr_t_service,pid) {
+			let fcfs_test = new Fcfs()
+			fcfs_test.showMessage()
+			this.mainObj.type = "FirstComeFirstServer(FCFS) / 先来先服务调度"
+			console.log(fcfs_test);
+			return fcfs_test.init(arr_name, arr_t_arrive, arr_t_service,pid)
+		},
+		InitTimePiece(arr_name, arr_t_arrive, arr_t_service,pid) {
+			let timepiece_test = new TimePiece()
+			timepiece_test.showMessage()
+			this.mainObj.type = "TimePiece(RR) / 时间片轮转调度"
+			console.log(timepiece_test);
+			return timepiece_test.init(arr_name, arr_t_arrive, arr_t_service,pid, this.time_piece)
+		},
+		doFcfs(arr_name, arr_t_arrive, arr_t_service,pid) {
+			let flag = true
+			for (let i = 0; i < pid.length; i++)
+				if (pid[i] == (undefined || null)) flag = false
+			for (let i = 0; i < arr_name.length; i++)
+				if (arr_name[i] == (undefined || null)) flag = false
+			for (let i = 0; i < arr_t_arrive.length; i++)
+				if (arr_t_arrive[i] == (undefined || null)) flag = false
+			for (let i = 0; i < arr_t_service.length; i++)
+				if (arr_t_service[i] == (undefined || null)) flag = false
+
+			if (flag) {
+				const {
+					resultArray,
+					finishArray
+				} = this.InitFcfs(arr_name, arr_t_arrive, arr_t_service,pid)
+
+				console.log('算法得到的结果',{
+					res: resultArray,
+					fin: finishArray
+				});
+				this.mainObj.res = resultArray
+				this.mainObj.tips = [
+					"先来先服务调度算法参数构成",
+					"1.上表的进程顺序按照进程进入顺序从上至下依次排列。",
+					"2.进程名称 指对应时间系统分配CPU等资源给哪一个进程，在标准 PCB 结构中，该参数对应着内外标识（唯一 PId）。",
+					"3.开始时间 指对应进程开始运行时在系统中对应的时间戳（相对）。",
+					"4.完成时间 指对应进程完成任务时在系统中对应的时间戳（相对）。",
+					"5.周转时间 指该进程在到达进程队列后需要多久才能完成任务。周转时间 = 完成时间 - 到达时间。",
+					"6.带权周转时间 指作业的周转时间与系统为它提供服务的时间之比，反映作业（或进程）长短问题。带权周转时间 = 周转时间 / 服务时间。"
+				]
+			}else{
+				window.alert("输入信息有误，请检查填写的用例信息。\n如果找不到错误建议点击用例设置中的“重置为原有测试用例”以消除错误\n\n一般引起本条警示的原因通常为：\n\t1.进程设置中有空值\n\t2.输入值过大或过小或为负值\n\t3.使用其他Js工具或语句进行阻塞")
+			}
+		},
+		doTimePiece(arr_name, arr_t_arrive, arr_t_service,pid) {
+			let flag = true
+			for (let i = 0; i < pid.length; i++)
+				if (pid[i] == (undefined || null)) flag = false
+			for (let i = 0; i < arr_name.length; i++)
+				if (arr_name[i] == (undefined || null)) flag = false
+			for (let i = 0; i < arr_t_arrive.length; i++)
+				if (arr_t_arrive[i] == (undefined || null)) flag = false
+			for (let i = 0; i < arr_t_service.length; i++)
+				if (arr_t_service[i] == (undefined || null)) flag = false
+			
+			if(flag){
+				const {
+					resultArray,
+					finishArray
+				} = this.InitTimePiece(arr_name, arr_t_arrive, arr_t_service,pid)
+				
+				console.log('算法得到的结果',{
+					res: resultArray,
+					fin: finishArray
+				});
+				this.mainObj.res = resultArray
+				this.mainObj.tips = [
+					"时间片轮转调度算法参数构成",
+					"1.上表中的每一行对应着在系统中运行的时间片记录的相关信息，按照时间片运行顺序从上至下依次排列。",
+					"2.进程名称 指对应时间片下系统分配CPU等资源给哪一个进程，在标准 PCB 结构中，该参数对应着内外标识（唯一 PId）。",
+					"3.开始时间 指对应时间片开始运行时在系统中对应的时间戳（相对）。",
+					"4.运行时间 指该进程在此时间片中运行的时间。",
+					"5.剩余服务时间 指该进程在除去当前时间片的任务调度之后，需要多久才能完成任务。",
+					"6.结束时间 指对应时间片结束运行时在系统中对应的时间戳（相对）。"
+				]
+			}else{
+				window.alert("输入信息有误，请检查填写的用例信息。\n如果找不到错误建议点击用例设置中的“重置为原有测试用例”以消除错误\n\n一般引起本条警示的原因通常为：\n\t1.进程设置中有空值\n\t2.输入值过大或过小或为负值\n\t3.使用其他Js工具或语句进行阻塞")
+			}
+		}
+	}
+};
+Vue.createApp(App).mount('#app')
