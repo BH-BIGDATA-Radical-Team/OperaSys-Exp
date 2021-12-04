7901da008bd8ac64d46c32198881047ce608ea82
NeserCode <73767966+NeserCode@users.noreply.github.com>
2021-12-04 20:31:06 +0800

    second upload
    keep update

--- js/myDefine.js
+++ js/myDefine.js
@@ -8,6 +8,7 @@ class Fcfs {
 	t_round = new Array()
 	t_round_vex = new Array()
 	finishArray = new Array()
+	resultArray = new Array()
 
 	constructor(arg) {
 		console.log('正在创建的是： 先来先服务 （FCFS）')
@@ -20,9 +21,7 @@ class Fcfs {
 		this.t_service = t_ser
 		this.pid = pid
 
-		let arr = new Array(),
-			result = new Array()
-
+		let arr = new Array()
 
 		console.time("fcfs")
 
@@ -31,6 +30,7 @@ class Fcfs {
 			b[0] = this.name[i]
 			b[1] = this.t_arrive[i]
 			b[2] = this.t_service[i]
+			b.push(this.pid[i])
 			arr[i] = b
 		}
 		arr.sort((a, b) => a[1] - b[1])
@@ -42,22 +42,24 @@ class Fcfs {
 			b[2] = parseFloat(Number(b[1] - arr[i][1]).toFixed(2))
 			b[3] = parseFloat(Number(b[2] / arr[i][2]).toFixed(2))
 
+			
+			this.finishArray.push({
+				name: arr[i][0],
+				pid: arr[i][3],
+				time: [b[0], b[1]]
+			})
+			arr[i].pop()
 			arr[i] = arr[i].concat(b)
-			this.finishArray.push(arr[i][0])
 			this.t_start.push(b[0])
 			this.t_finish.push(b[1])
 			this.t_round.push(b[2])
 			this.t_round_vex.push(b[3])
 		}
 
-		result = arr
-
 		console.log('↓ 本算法程序用时 ↓');
 		console.timeEnd("fcfs")
-		return {
-			resultArray: result,
-			finishArray: this.finishArray
-		}
+		this.resultArray = arr
+		return this
 	}
 
 	showMessage() {
@@ -81,6 +83,7 @@ class TimePiece {
 	t_subService = new Array()
 	t_finish = new Array()
 	finishArray = new Array()
+	resultArray = new Array()
 
 	constructor(arg) {
 		console.log('正在创建的是： 时间片轮转调度 （TimePiece）');
@@ -97,8 +100,7 @@ class TimePiece {
 		let arr = new Array(),
 			result = new Array(),
 			finish_num = 0,
-			k = 0,
-			time_counter = 0
+			k = 0
 
 		console.time("timepiece")
 
@@ -111,6 +113,7 @@ class TimePiece {
 			b[1] = this.t_arrive[i]
 			b[2] = this.t_service[i]
 			b[3] = false
+			b.push(this.pid[i])
 			arr[i] = b
 		}
 		//将数据由到达时间排序
@@ -131,7 +134,11 @@ class TimePiece {
 					b[4] = b[1] + b[2]
 					if (b[3] <= 0) {
 						arr[i][3] = true
-						this.finishArray.push(arr[i][0])
+						this.finishArray.push({
+							name: arr[i][0],
+							pid: arr[i][4],
+							time: [b[1], b[1] + b[2]]
+						})
 						finish_num++
 					}
 					arr[i][2] -= b[2]
@@ -145,18 +152,114 @@ class TimePiece {
 
 			}
 		}
-		console.log('↓ 本算法程序用时 ↓');
+		console.log('↓ 本算法程序用时 ↓')
 		console.timeEnd("timepiece")
-		return {
-			resultArray: result,
-			finishArray: this.finishArray
-		}
+		this.resultArray = result
+		return this
 	}
 
 	showMessage() {
 		console.log(`您创建了一个实例，此实例将用作演示 时间片轮转算法(TimePiece)`);
 		console.log(`调用该实例的 INIT（）函数进行一次调度算法模拟,也可以在控制台中输入 TimePiece 查看全局变量。`);
 		console.log(`例如 \nlet a = new TimePiece()\n之后在控制台键入 a 即可看到返回值`);
+	}
+
+}
+
+class DynamicPriority {
+
+	time_rest = new Number()
+
+	name = new Array()
+	pid = new Array()
+	priority = new Array()
+	t_arrive = new Array()
+	t_service = new Array()
+	t_start = new Array()
+	t_runin = new Array()
+	t_subService = new Array()
+	t_finish = new Array()
+	finishArray = new Array()
+	resultArray = new Array()
+
+	constructor(arg) {
+		console.log('正在创建的是： 动态优先级调度 （DynamicPriority）');
+	}
+
+	init(name, t_arr, t_ser, prio, pid, tp) {
+
+		this.name = name
+		this.t_arrive = t_arr
+		this.t_service = t_ser
+		this.priority = prio
+		this.pid = pid
+		this.time_rest = tp
+
+		let arr = new Array(),
+			result = new Array(),
+			finish_num = 0,
+			k = 0
+
+		console.time("dynamicpriority")
+
+		for (let i = 0; i < this.name.length; i++) {
+			let b = new Array()
+
+			// 进程名|到达时间|服务时间|完成状态|优先级
+			b[0] = this.name[i]
+			b[1] = this.t_arrive[i]
+			b[2] = this.t_service[i]
+			b[3] = false
+			b[4] = prio[i]
+			arr[i] = b
+		}
+
+		while (finish_num < this.name.length) {
+
+			let b = new Array()
+
+			arr.sort((a, b) => a[4] - b[4])
+
+			if (!arr[0][3]) {
+				// Array b[] 进程名|运行时间|剩余服务时间|当前优先级
+				// Array arr[][] 进程名|到达时间|服务时间|优先级|完成状态(true/false)
+
+				b[0] = arr[0][0]
+				b[1] = arr[0][2] >= tp ? tp : arr[0][2]
+				b[2] = arr[0][2] - b[1]
+				b[3] = arr[0][4]
+				if (b[2] <= 0) {
+					arr[0][3] = true
+					arr[0][4] = 9999999
+					this.finishArray.push({
+						name: arr[0][0],
+						time: [(k == 0 ? arr[0][1] : this.t_finish[k - 1]), b[1] + (k == 0 ? arr[0][1] :
+							this.t_finish[k - 1])]
+					})
+					finish_num++
+				}
+				arr[0][2] -= b[1]
+				arr[0][4] += 1
+				result.push(b)
+				this.t_start.push(k == 0 ? arr[0][1] : this.t_finish[k - 1])
+				this.t_runin.push(b[1])
+				this.t_subService.push(b[2])
+				this.t_finish.push(b[1] + (k == 0 ? arr[0][1] : this.t_finish[k - 1]))
+				k++
+			}
+
+		}
+
+		console.log('↓ 本算法程序用时 ↓');
+		console.timeEnd("dynamicpriority")
+		this.resultArray = result
+		return this
+	}
+
+	showMessage() {
+		console.log(`您创建了一个实例，此实例将用作演示 动态优先级算法(DynamicPriority)`);
+		console.log(`调用该实例的 INIT（）函数进行一次调度算法模拟,也可以在控制台中输入 DynamicPriority 查看全局变量。`);
+		console.log(`例如 \nlet a = new DynamicPriority()\n之后在控制台键入 a 即可看到返回值`);
 	}
 
 }
--- js/myVue.js
+++ js/myVue.js
@@ -6,13 +6,23 @@ const App = {
 			testCase_name: ['进程1', '进程2', '进程3', '进程4', '进程5'],
 			testCase_arriveTime: [3, 2, 4, 6, 5],
 			testCase_serviceTime: [4, 5, 6, 4, 5],
+			testCase_priority: [2, 1, 4, 3, 5],
 			time_piece: 2,
+			time_rest: 2,
+			isDarkScheme: true,
+			isShowTips: true,
+			isShowTestCase: false,
 			mainObj: {
 				type: null,
 				res: null,
 				tips: [],
 			},
-			isShowTestCase: true,
+			rp: {},
+			rp_time: 2,
+			display_rp:{
+				pid:[],
+				name:[],
+			},
 		}
 	},
 	mounted() {
@@ -22,7 +32,7 @@ const App = {
 	computed: {
 		type_display: function() {
 			return this.mainObj.type ?? "↓ 您还没有选择一种算法 ↓"
-		}
+		},
 	},
 	methods: {
 		InitApp() {
@@ -39,55 +49,84 @@ const App = {
 				testCase_name = ['进程1', '进程2', '进程3', '进程4', '进程5'],
 				testCase_arriveTime = [3, 2, 4, 6, 5],
 				testCase_serviceTime = [4, 5, 6, 4, 5],
-				time_piece = 2
+				testCase_priority = [2, 1, 4, 3, 5],
+				time_piece = 2,
+				time_rest = 2
 
 			this.testCase_pid = testCase_pid
 			this.testCase_name = testCase_name
 			this.testCase_arriveTime = testCase_arriveTime
 			this.testCase_serviceTime = testCase_serviceTime
+			this.testCase_priority = testCase_priority
 			this.time_piece = time_piece
+			this.time_rest = time_rest
 
 			console.log("实验用例已重置");
-
 		},
 		insertTestCase() {
 			this.testCase_pid.push(null)
 			this.testCase_name.push(null)
 			this.testCase_arriveTime.push(null)
 			this.testCase_serviceTime.push(null)
+			this.testCase_priority.push(null)
 		},
-		InitFcfs(arr_name, arr_t_arrive, arr_t_service,pid) {
+		toggleColorScheme() {
+			let r = document.querySelector(":root"),
+				bgc = this.isDarkScheme ? `rgba(0,0,0,.85)` : `rgba(255,255,255,.8)`,
+				fc = this.isDarkScheme ? `rgba(255,255,255,.8)` : `rgba(0,0,0,.85)`,
+				bgbc = this.isDarkScheme ? `rgba(255,255,255,.15)` : `rgba(0,0,0,.05)`,
+				tsc = this.isDarkScheme ? `rgba(255,255,255,.05)` : `rgba(0,0,0,.05)`
+
+			r.style.setProperty('--bg-color', bgc)
+			r.style.setProperty('--font-color', fc)
+			r.style.setProperty('--bg-button-color', bgbc)
+			r.style.setProperty('--table-set-color', tsc)
+
+			this.isDarkScheme = !this.isDarkScheme
+		},
+		InitFcfs(arr_name, arr_t_arrive, arr_t_service, pid) {
 			let fcfs_test = new Fcfs()
 			fcfs_test.showMessage()
 			this.mainObj.type = "FirstComeFirstServer(FCFS) / 先来先服务调度"
 			console.log(fcfs_test);
-			return fcfs_test.init(arr_name, arr_t_arrive, arr_t_service,pid)
+			return fcfs_test.init(arr_name, arr_t_arrive, arr_t_service, pid)
 		},
-		InitTimePiece(arr_name, arr_t_arrive, arr_t_service,pid) {
+		InitTimePiece(arr_name, arr_t_arrive, arr_t_service, pid) {
 			let timepiece_test = new TimePiece()
 			timepiece_test.showMessage()
 			this.mainObj.type = "TimePiece(RR) / 时间片轮转调度"
 			console.log(timepiece_test);
-			return timepiece_test.init(arr_name, arr_t_arrive, arr_t_service,pid, this.time_piece)
+			return timepiece_test.init(arr_name, arr_t_arrive, arr_t_service, pid, this.time_piece)
 		},
-		doFcfs(arr_name, arr_t_arrive, arr_t_service,pid) {
+		InitDynamicPriority(arr_name, arr_t_arrive, arr_t_service, arr_prio, pid) {
+			let dynamicpriority_test = new DynamicPriority()
+			dynamicpriority_test.showMessage()
+			this.mainObj.type = "DynamicPriority / 动态优先级调度"
+			console.log(dynamicpriority_test);
+			return dynamicpriority_test.init(arr_name, arr_t_arrive, arr_t_service, arr_prio, pid, this.time_rest)
+		},
+		doFcfs(arr_name, arr_t_arrive, arr_t_service, pid) {
 			let flag = true
 			for (let i = 0; i < pid.length; i++)
-				if (pid[i] == (undefined || null)) flag = false
+				if (pid[i] == undefined || pid[i] == null || pid[i] == "") flag = false
 			for (let i = 0; i < arr_name.length; i++)
-				if (arr_name[i] == (undefined || null)) flag = false
+				if (arr_name[i] == undefined || arr_name[i] == null || arr_name[i] == "") flag = false
 			for (let i = 0; i < arr_t_arrive.length; i++)
-				if (arr_t_arrive[i] == (undefined || null)) flag = false
+				if (arr_t_arrive[i] == undefined || arr_t_arrive[i] == null || arr_t_arrive[i] == "") flag = false
 			for (let i = 0; i < arr_t_service.length; i++)
-				if (arr_t_service[i] == (undefined || null)) flag = false
+				if (arr_t_service[i] == undefined || arr_t_service[i] == null || arr_t_service[i] == "") flag =
+					false
+
+			console.log(pid.length, arr_name, arr_t_arrive, arr_t_service);
 
 			if (flag) {
+				this.rp = this.InitFcfs(arr_name, arr_t_arrive, arr_t_service, pid)
 				const {
 					resultArray,
 					finishArray
-				} = this.InitFcfs(arr_name, arr_t_arrive, arr_t_service,pid)
+				} = this.rp
 
-				console.log('算法得到的结果',{
+				console.log('算法得到的结果', {
 					res: resultArray,
 					fin: finishArray
 				});
@@ -101,28 +140,34 @@ const App = {
 					"5.周转时间 指该进程在到达进程队列后需要多久才能完成任务。周转时间 = 完成时间 - 到达时间。",
 					"6.带权周转时间 指作业的周转时间与系统为它提供服务的时间之比，反映作业（或进程）长短问题。带权周转时间 = 周转时间 / 服务时间。"
 				]
-			}else{
-				window.alert("输入信息有误，请检查填写的用例信息。\n如果找不到错误建议点击用例设置中的“重置为原有测试用例”以消除错误\n\n一般引起本条警示的原因通常为：\n\t1.进程设置中有空值\n\t2.输入值过大或过小或为负值\n\t3.使用其他Js工具或语句进行阻塞")
+				this.initRP()
+			} else {
+				window.alert(
+					"输入信息有误，请检查填写的用例信息。\n如果找不到错误建议点击用例设置中的“重置为原有测试用例”以消除错误\n\n一般引起本条警示的原因通常为：\n\t1.进程设置中有空值\n\t2.输入值过大或过小或为负值\n\t3.使用其他Js工具或语句进行阻塞"
+				)
 			}
 		},
-		doTimePiece(arr_name, arr_t_arrive, arr_t_service,pid) {
+		doTimePiece(arr_name, arr_t_arrive, arr_t_service, pid) {
 			let flag = true
 			for (let i = 0; i < pid.length; i++)
-				if (pid[i] == (undefined || null)) flag = false
+				if (pid[i] == undefined || pid[i] == null || pid[i] == "") flag = false
 			for (let i = 0; i < arr_name.length; i++)
-				if (arr_name[i] == (undefined || null)) flag = false
+				if (arr_name[i] == undefined || arr_name[i] == null || arr_name[i] == "") flag = false
 			for (let i = 0; i < arr_t_arrive.length; i++)
-				if (arr_t_arrive[i] == (undefined || null)) flag = false
+				if (arr_t_arrive[i] == undefined || arr_t_arrive[i] == null || arr_t_arrive[i] == "") flag = false
 			for (let i = 0; i < arr_t_service.length; i++)
-				if (arr_t_service[i] == (undefined || null)) flag = false
-			
-			if(flag){
+				if (arr_t_service[i] == undefined || arr_t_service[i] == null || arr_t_service[i] == "") flag =
+					false
+			if (this.time_piece == undefined || this.time_piece == null || this.time_piece == "") flag = false
+
+			if (flag) {
+				this.rp = this.InitTimePiece(arr_name, arr_t_arrive, arr_t_service, pid)
 				const {
 					resultArray,
 					finishArray
-				} = this.InitTimePiece(arr_name, arr_t_arrive, arr_t_service,pid)
-				
-				console.log('算法得到的结果',{
+				} = this.rp
+
+				console.log('算法得到的结果', {
 					res: resultArray,
 					fin: finishArray
 				});
@@ -136,9 +181,71 @@ const App = {
 					"5.剩余服务时间 指该进程在除去当前时间片的任务调度之后，需要多久才能完成任务。",
 					"6.结束时间 指对应时间片结束运行时在系统中对应的时间戳（相对）。"
 				]
-			}else{
-				window.alert("输入信息有误，请检查填写的用例信息。\n如果找不到错误建议点击用例设置中的“重置为原有测试用例”以消除错误\n\n一般引起本条警示的原因通常为：\n\t1.进程设置中有空值\n\t2.输入值过大或过小或为负值\n\t3.使用其他Js工具或语句进行阻塞")
+				this.initRP()
+			} else {
+				window.alert(
+					"输入信息有误，请检查填写的用例信息。\n如果找不到错误建议点击用例设置中的“重置为原有测试用例”以消除错误\n\n一般引起本条警示的原因通常为：\n\t1.进程设置中有空值\n\t2.输入值过大或过小或为负值\n\t3.使用其他Js工具或语句进行阻塞"
+				)
 			}
+		},
+		doDynamicPriority(arr_name, arr_t_arrive, arr_t_service, arr_priority, pid) {
+			let flag = true
+			for (let i = 0; i < pid.length; i++)
+				if (pid[i] == undefined || pid[i] == null || pid[i] == "") flag = false
+			for (let i = 0; i < arr_name.length; i++)
+				if (arr_name[i] == undefined || arr_name[i] == null || arr_name[i] == "") flag = false
+			for (let i = 0; i < arr_t_arrive.length; i++)
+				if (arr_t_arrive[i] == undefined || arr_t_arrive[i] == null || arr_t_arrive[i] == "") flag = false
+			for (let i = 0; i < arr_t_service.length; i++)
+				if (arr_t_service[i] == undefined || arr_t_service[i] == null || arr_t_service[i] == "") flag =
+					false
+			for (let i = 0; i < arr_priority.length; i++)
+				if (arr_priority[i] == undefined || arr_priority[i] == null || arr_priority[i] == "") flag = false
+
+			if (flag) {
+				this.rp = this.InitDynamicPriority(arr_name, arr_t_arrive, arr_t_service, arr_priority, pid)
+				const {
+					resultArray,
+					finishArray
+				} = this.rp
+
+				console.log('算法得到的结果', {
+					res: resultArray,
+					fin: finishArray
+				});
+				this.mainObj.res = resultArray
+				this.mainObj.tips = [
+					"动态优先级调度算法参数构成",
+					"1.上表中的每一行对应着在系统中运行的时间间隔中进程的相关信息，按照时间顺序从上至下依次排列。",
+					"2.进程名称 指对应时刻系统分配CPU等资源给哪一个进程，在标准 PCB 结构中，该参数对应着内外标识（唯一 PId）。",
+					"3.运行时间 指该进程在此当前时间间隔中运行的时间。",
+					"4.剩余服务时间 指该进程在除去当前阶段的作业之后，需要多久才能完成作业。",
+					"5.优先级 指对应时刻对应进程在系统中对应的优先级（相对）。"
+				]
+				this.initRP()
+			} else {
+				window.alert(
+					"输入信息有误，请检查填写的用例信息。\n如果找不到错误建议点击用例设置中的“重置为原有测试用例”以消除错误\n\n一般引起本条警示的原因通常为：\n\t1.进程设置中有空值\n\t2.输入值过大或过小或为负值\n\t3.使用其他Js工具或语句进行阻塞"
+				)
+			}
+		},
+		initRP(){
+			for(let i=0;i<this.rp.finishArray.length;i++){
+				this.display_rp.pid.push(this.rp.finishArray[i].pid)
+				this.display_rp.name.push(this.rp.finishArray[i].name)
+			}
+		},
+		handleRPStatus(index){
+			let start = this.rp.finishArray[index].time[0],
+				end = this.rp.finishArray[index].time[1]
+				
+			
+			
+			if(this.rp_time < start) return 'wait'
+			else if(this.rp_time == start) return 'start'
+			else if(this.rp_time > start && this.rp_time < end) return 'running'
+			else if(this.rp_time == end) return 'ending'
+			else return 'ended'
 		}
 	}
 };
